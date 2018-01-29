c5d4371b679f43bdb20664caea36eac9a0e29f41
Angel Anton <angel.anton@monday8am.com>
2018-01-29 13:15:59 +0100

    Added class_5 file.

---
+++ class_5.py
@@ -0,0 +1,19 @@
+# Procedures
+
+# Define a procedure, sum3, that takes three
+# inputs, and returns the sum of the three
+# input numbers.
+# To help you out, the code for sum(a,b) is below.
+
+# Example:
+
+print sum3(1,2,3) # >>> 6 
+print sum3(93,53,70) # >>> 216
+
+# Answer:
+
+def sum3(a, b, c): return a + b + c
+
+x=1  y=2  z=3
+
+print sum3(x, y, z)
